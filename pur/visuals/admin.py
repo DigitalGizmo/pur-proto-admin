0703023f80edfffd4237335307ae4e6bb9e5f762
Don Button <donpublic@digitalgizmo.com>
2021-06-17 10:39:51 -0400

    improve Image field order

--- pur/visuals/admin.py
+++ pur/visuals/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Image
 
 class ImageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['orig_filename', 'title', 'slug',   
+            'description', 'alt_text', ('creation_year', 'circa'), 'source',
+            'location', 'persons', ]}),
+    ]
     filter_horizontal = ['persons']
 
 admin.site.register(Image, ImageAdmin)
