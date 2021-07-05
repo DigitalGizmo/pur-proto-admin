cd619afae2ac8d7c9fbc832685f2427b970daae1
Don Button <donpublic@digitalgizmo.com>
2021-07-05 16:50:58 -0400

    finish changeing Source in visusals

--- pur/visuals/admin.py
+++ pur/visuals/admin.py
@@ -5,10 +5,13 @@ from .models import Image, Topic, Source
 class ImageAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,  {'fields': ['orig_filename', 'title', 'slug',   
-            'description', 'alt_text', ('creation_year', 'circa'), 'source',
+            'description', 'alt_text', ('creation_year', 'circa', 'decade'), 
+            'source', 'topics',
             'location', 'persons', ]}),
+        ('Behind the scenes',   {'fields': ['status_num', 'priority']}), 
+            # , 'classes': ['collapse']
     ]
-    filter_horizontal = ['persons']
+    filter_horizontal = ['persons', 'topics']
     list_display = ('title', 'slug', 'image_img', 'orig_filename', 'location',
         )
 
