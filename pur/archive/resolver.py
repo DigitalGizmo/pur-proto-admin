bc574f285d57ef165c45699a072135ac5c736a3d
Don Button <donpublic@digitalgizmo.com>
2021-10-10 17:34:20 -0400

    add city_id query to graphql

--- pur/archive/resolver.py
+++ pur/archive/resolver.py
@@ -2,24 +2,26 @@ from ariadne import QueryType, make_executable_schema
 from .models import ArchiveItem
 
 type_defs= """
-
 type Query{
-    all_images: [ArchiveItem]
+    all_images(city_id: Int): [ArchiveItem]
 }
-
 type ArchiveItem {
-    id: ID
+    id: Int
     slug: String!
     title: String!
 
 }
-
 """
 
 query = QueryType()
 
 @query.field('all_images')
-def resolve_archive_images(*_):
-    return ArchiveItem.objects.filter(media_format__media_type__slug='image')
+def resolve_archive_images(*_, city_id=None): # city=None
+    print("city_id: " + str(city_id))
+    if city_id:
+        return ArchiveItem.objects.filter(media_format__media_type__slug='image',
+            status_num__gte=2, priority__gte=1, location__city=city_id)
+    return ArchiveItem.objects.filter(media_format__media_type__slug='image',
+            status_num__gte=2, priority__gte=1)
 
 schema = make_executable_schema(type_defs, query)
