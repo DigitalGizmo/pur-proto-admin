ccfcd80c2421cdcb3c35c14beb18673359431be8
Don Button <donpublic@digitalgizmo.com>
2021-10-30 16:56:22 -0400

    Update Interactives admin

--- pur/interactives/admin.py
+++ pur/interactives/admin.py
@@ -1,22 +1,25 @@
 from django.contrib import admin
 
-# from .models import Interactive, Hotspot
+from .models import Interactive, Hotspot
 
-# class InteractivesAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None, {'fields': ['city', 'slug', 'title', 'blurb']
-#         }),
-#     ]
-#     list_display = ('city', 'slug', 'title')
+class InteractivesAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['city', 'slug', 'title', 'blurb']
+        }),
+    ]
+    list_display = ('city', 'slug', 'title')
 
-# class HotspotAdmin(admin.ModelAdmin):
-#     fieldsets = [
-#         (None, {'fields': ['ordinal', 'title', 
-#             ('text_percent', 'hotspot_x', 'hotspot_y', 'hotspot_r', 
-#                 'blurb', 'more')
-#             ]
-#         }),
-#     ]
-#     list_display = ('ordinal', 'title', 
-#         'text_percent', 'hotspot_x', 'hotspot_y', 'hotspot_r')
-#     list_filter = ['']
+class HotspotAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['interactive', 'ordinal', 'title', 
+            ('text_percent', 'hotspot_x', 'hotspot_y', 'hotspot_r', 
+                'blurb', 'more')
+            ]
+        }),
+    ]
+    list_display = ('ordinal', 'title', 
+        'text_percent', 'hotspot_x', 'hotspot_y', 'hotspot_r')
+    list_filter = ['interactive']
+
+admin.site.register(Interactive ,InteractivesAdmin)
+admin.site.register(Hotspot, HotspotAdmin)
