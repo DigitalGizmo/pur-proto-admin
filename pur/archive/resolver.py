2d95258b43bb3d20b89f46f54482fccb1211a5c4
Don Button <donpublic@digitalgizmo.com>
2021-11-01 12:43:08 -0400

    add hotspots to archive graphql

--- pur/archive/resolver.py
+++ pur/archive/resolver.py
@@ -1,9 +1,11 @@
 from ariadne import QueryType, make_executable_schema
 from .models import ArchiveItem
+from pur.interactives.models import Hotspot
 
 type_defs= """
 type Query{
     all_images(city_id: Int): [ArchiveItem]
+    hotspots(interactive_id: Int): [Hotspot]
 }
 type ArchiveItem {
     id: Int
@@ -15,6 +17,16 @@ type ArchiveItem {
     city: String
     district_title: String
     street_address: String
+}
+type Hotspot {
+    id: Int
+    ordinal: Int
+    title: String!
+    blurb: String
+    text_percent: Int
+    hotspot_x: Int
+    hotspot_y: Int
+    hotspot_r: Int
 }
 """
 
@@ -28,5 +40,12 @@ def resolve_archive_images(*_, city_id=None): # city=None
             status_num__gte=2, priority__gte=1, city=city_id)
     return ArchiveItem.objects.filter(media_format__media_type__slug='image',
             status_num__gte=2, priority__gte=1)
+
+@query.field('hotspots')
+def resolve_hotspots(*_, interactive_id=None): # city=None
+    print("interactive_id: " + str(interactive_id))
+    if interactive_id:
+        return Hotspot.objects.filter(interactive=interactive_id)
+    return Hotspot.objects.all()
 
 schema = make_executable_schema(type_defs, query)
