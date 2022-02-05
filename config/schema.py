5e9475387b49c68acf5c537891b84608c7cab1b8
Don Button <donpublic@digitalgizmo.com>
2022-02-05 16:31:17 -0500

    central straw, clearing ariadne

--- config/schema.py
+++ config/schema.py
@@ -1,9 +1,11 @@
 import strawberry
 from typing import List
-from .types import Hotspot
+from pur.interactives.types import Hotspot
+from pur.archive.types import ArchiveItem
 
 @strawberry.type
 class Query:
   hotspots: List[Hotspot] = strawberry.django.field()
+  all_images: List[ArchiveItem] = strawberry.django.field()
 
 schema = strawberry.Schema(query=Query)
--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,12 @@
-# from ariadne_django.views import GraphQLView
 from strawberry.django.views import GraphQLView
 from django.contrib import admin
 from django.urls import include, path
-# from .graphql_config import schema
-from pur.interactives.schema import schema
+from .schema import schema
 from pur import people
 
 urlpatterns = [
     path('people/', include('pur.people.urls')),
     path('archive/', include('pur.archive.urls')),
-    path('interactives/', include('pur.interactives.urls')),
     path('graphql/', GraphQLView.as_view(schema=schema), name='graphql'),
-    # path('graphql/', GraphQLView.as_view(schema=schema), name='graphql'),
     path('admin/', admin.site.urls),
 ]
