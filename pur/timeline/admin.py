a97e7351b324096415424f13869f54160fc92ea7
Don Button <donpublic@digitalgizmo.com>
2022-03-18 11:08:23 -0400

    timeline layer, thruline admin

--- pur/timeline/admin.py
+++ pur/timeline/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import TimelineLayer, Thruline
 
-# Register your models here.
+class TimelineLayerAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': [('slug', 'ordianl'), 'title' ]})
+  ]
+list_display = ('title', 'ordinal', 'slug')
+
+class ThrulineAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': [('slug', 'ordianl'), 'title' ]})
+  ]
+list_display = ('title', 'ordinal', 'slug')
+
+admin.site.register(TimelineLayer, TimelineLayerAdmin)
+admin.site.register(Thruline, ThrulineAdmin)
